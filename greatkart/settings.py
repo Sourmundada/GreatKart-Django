b1486b2912e8f3413d53130141e4e6f6cab1acfa
Sourmundada <69742346+Sourmundada@users.noreply.github.com>
2021-04-11 12:10:35 +0530

    update "settings.py"

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,5 +143,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'saurabhmundada111@gmail.com'
-EMAIL_HOST_PASSWORD = 'saurabh1234'
+EMAIL_HOST_PASSWORD = '*********'
 EMAIL_USE_TLS = True
